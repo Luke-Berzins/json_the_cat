946c501bbe24d6e1ed68f8022b50902b9a4a0042
luke-berzins <lukeberzins16@gmail.com>
2020-12-03 17:51:06 +0000

    cleanup

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require('request');
 
-const myArgs = process.argv.slice(2);
-const breed = myArgs[0];
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (body === undefined) {
+      error = "request from site failed";
+      callback(error, null);
+    }
+    if (body !== undefined) {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        error = "invalid breed";
+      }
+      if (error) {
+        callback(error, null);
+      }
+      if (!error) {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was
-  }
-  if (body === undefined) {
-    return console.log("err: request failed");
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("error: input incorrect");
-  } else {
-    console.log(data[0].description);
-  }
-});
 
-
+module.exports = { fetchBreedDescription };
